64ad41d8b41805bd9dcf15e2802f1382e72d2659
Paul Onteri <onteripaul@gmail.com>
2020-12-01 14:30:15 +0300

    note models

--- Backend/notes/admin.py
+++ Backend/notes/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Note, NoteUser
 
-# Register your models here.
+admin.site.register(Note)
+admin.site.register(NoteUser)
--- Backend/notes/models.py
+++ Backend/notes/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+
+    class Meta:
+        ordering = ['-title']
+
+    def __str__(self):
+        return self.title
+
+
+class NoteUser(models.Model):
+    note = models.ForeignKey(Note, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
