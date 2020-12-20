458f2f5c42a2e4e316a6073dc0661a6afedbbc55
Paul Onteri <onteripaul@gmail.com>
2020-12-20 03:19:07 +0300

    Header

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import store from "./state/store";
 import { Route, HashRouter, Switch } from "react-router-dom";
 //
+import Header from "./components/Header";
 import Note from "./components/notes/Note";
 import EditNote from "./components/notes/EditNote";
 import Login from "./components/Login";
@@ -12,20 +13,23 @@ import NotesList from "./components/notes/NotesList";
 
 function App() {
   return (
-    <Provider store={store}>
-      <HashRouter>
-        <Switch>
-          <Route exact path="/note" component={Note} />
-          <Route exact path="/note/:noteId/edit" component={EditNote} />
-          <Route exact path="/note/:noteId" component={Note} />
-          <Route exact path="/notes" component={NotesList} />
-          <Route exact path="/home" component={HomePage} />
-          <Route exact path="/login" component={Login} />
-          {/* <PrivateRoute path="/" component={Dashboard} /> */}
-          <Route exact path="/" component={Login} />
-        </Switch>
-      </HashRouter>
-    </Provider>
+    <>
+      <Provider store={store}>
+        <HashRouter>
+          <Header />
+          <Switch>
+            <Route exact path="/note" component={Note} />
+            <Route exact path="/note/:noteId/edit" component={EditNote} />
+            <Route exact path="/note/:noteId" component={Note} />
+            <Route exact path="/notes" component={NotesList} />
+            <Route exact path="/home" component={HomePage} />
+            <Route exact path="/login" component={Login} />
+            {/* <PrivateRoute path="/" component={Dashboard} /> */}
+            <Route exact path="/" component={Login} />
+          </Switch>
+        </HashRouter>
+      </Provider>
+    </>
   );
 }
 
---
+++ Frontend/src/components/Header.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles({
+  root: {
+    margin: 5,
+    marginBottom: 10,
+    // backgroundColor: "red",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    flex: 1,
+  },
+  buttons: {
+    flex: 1.3,
+    // backgroundColor: "green",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+  button: {
+    margin: 5,
+  },
+});
+
+const Header = () => {
+  const classes = useStyles();
+  return (
+    <Container className={classes.root}>
+      <Container style={{ flex: 0.2 }}>
+        <p>Notes App</p>
+      </Container>
+      <Container className={classes.buttons}>
+        <Link to={`/note`}>
+          <Button className={classes.button} variant="contained">
+            Shared Notes
+          </Button>
+        </Link>
+        <Link to={`/note`}>
+          <Button className={classes.button} variant="contained">
+            New Note
+          </Button>
+        </Link>
+      </Container>
+    </Container>
+  );
+};
+
+export default Header;
