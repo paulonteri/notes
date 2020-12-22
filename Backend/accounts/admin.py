c277f643d10ee3e8bf2c56109610559a5cd9aa50
Paul Onteri <onteripaul@gmail.com>
2020-12-22 11:12:39 +0300

    Users admin

--- Backend/accounts/admin.py
+++ Backend/accounts/admin.py
@@ -8,23 +8,26 @@ class UserAdmin(UserAdmin):
     # Display
     fieldsets = (
         (None, {'fields': ('username', 'email', 'password')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'is_admin',)}),
+        ('Permissions', {'fields': ('is_staff',
+                                    'is_active', 'is_superuser', 'is_admin',)}),
     )
 
     # Create User
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'email', 'password1', 'password2'),
+            'fields': ('username', 'email', 'first_name', 'last_name', 'password1', 'password2'),
         }),
-        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'is_admin',)}),
+        ('Permissions', {'fields': ('is_staff', 'role',
+                                    'is_active', 'is_superuser', 'is_admin',)}),
     )
 
     # List display
-    list_display = ('username', 'email', 'is_active', 'is_admin', 'is_staff',)
-    list_filter = ('is_admin', 'is_active', 'is_superuser', 'is_staff',)
+    list_display = ('username', 'email', 'first_name',
+                    'last_name', 'is_admin', 'role', 'date_joined')
+    list_filter = ('is_admin', 'role', 'is_staff', 'date_joined')
 
-    search_fields = ('username', 'email',)
+    search_fields = ('username', 'email', 'first_name', 'last_name')
     ordering = ('username', 'email',)
 
 
