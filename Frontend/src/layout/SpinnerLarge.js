31749a0154c30cd7658608c1765458a62a844adf
Paul Onteri <onteripaul@gmail.com>
2020-12-22 15:06:31 +0300

    spinner position

--- Frontend/src/layout/SpinnerLarge.js
+++ Frontend/src/layout/SpinnerLarge.js
@@ -1,14 +1,28 @@
 import React from "react";
 import MoonLoader from "react-spinners/MoonLoader";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    width: window.screen.width / 1.05,
+    height: window.screen.height / 1.4,
+    margin: 5,
+    marginBottom: 10,
+    // backgroundColor: "red",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 const SpinnerLarge = () => {
+  const classes = useStyles();
+
   return (
-    <div
-      className="container-fluid d-flex justify-content-center align-items-center"
-      style={{ height: "80vh" }}
-    >
+    <Container className={classes.root}>
       <MoonLoader size={70} color={"#123abc"} />
-    </div>
+    </Container>
   );
 };
 
